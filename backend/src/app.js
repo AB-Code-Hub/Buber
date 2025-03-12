b5bba89f70e0518729f7d65773dd713de06af60b
Bilal <mobilal560@gmail.com>
2025-03-12 12:55:18 +0530

    implement user registration with validation; add user service and routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,11 +2,17 @@ import express from 'express'
 import cors from 'cors'
 export const app = express()
 import {dbConnect} from "./db/db.js"
+import { router as userRoutes } from './routes/user.route.js'
 dbConnect();
 app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
 
 app.get('/', (req, res) => {
     res.send("Hello World")
 })
+
+app.use('/users', userRoutes)
 
 
--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,1 +1,30 @@
 import User from "../models/user.model.js";
+import { createUserService } from "../services/user.service.js";
+import { validationResult } from "express-validator";
+export const registerUser = async (req, res, next) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { fullName, email, password } = req.body;
+
+    const hashedPassword = await User.hashPassword(password);
+
+    const user = await createUserService({
+      firstName: fullName.firstName,
+      lastName: fullName.lastName,
+      email,
+      password: hashedPassword,
+    });
+
+    const token = user.generateAuthToken()
+
+    res.status(201).json({token: token, data: user})
+
+  } catch (error) {
+    console.error("Error in User controller", error)
+    return res.status(500).json({message: "Internal server error", error: error.message})
+  }
+};
